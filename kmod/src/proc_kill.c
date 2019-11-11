cd915019f4ac42076f672bd710473d93623700ed
Thomas Lam <z5136612@unsw.edu.au>
2019-11-11 21:08:09 +1100

    Fix breakage for newer kernels
    struct siginfo is replaced with struct kernel_siginfo in kernel v4.20 or
    later.

--- kmod/src/proc_kill.c
+++ kmod/src/proc_kill.c
@@ -1,6 +1,7 @@
 #include <linux/types.h>
 #include <linux/sched/signal.h>
 #include <linux/sched.h>
+#include <linux/version.h>
 #include <asm/signal.h>
 #include <asm-generic/siginfo.h>
 
@@ -10,11 +11,19 @@
 int proc_kill(pid_t pid)
 {
 	int signum = SIGKILL;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0)
 	struct siginfo info;
+#else
+	struct kernel_siginfo info;
+#endif
 	struct task_struct *task = find_task_from_pid(pid);
 	if (!task)
 		return -ESRCH;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0)
 	memset(&info, 0, sizeof(struct siginfo));
+#else
+	memset(&info, 0, sizeof(struct kernel_siginfo));
+#endif
 	info.si_signo = signum;
 
 	return send_sig_info(signum, &info, task);
