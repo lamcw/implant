9b01bdfc527fa00ae301ed4e78c9cfbcbda2eb2d
Nathan Lam <z5113345@unsw.edu.au>
2019-11-23 18:23:33 +1100

    Remove tests for file hiding

--- client/test/hide/0/2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./sc-client hide --file tmp
-echo >> ${TEST_PATH}
-diff -Z ${TEST_PATH} client/test/hide/0/2.comm
--- client/test/unhide/0/2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./sc-client unhide --file tmp
-echo >> ${TEST_PATH}
-diff -Z ${TEST_PATH} client/test/unhide/0/2.comm
